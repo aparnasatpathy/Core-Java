67e9f0cf1ce94f4a72a6587c84fb3a1515021e48
aparnasatpathy <satpathyaparna3@gmail.com>
2025-07-29 13:04:28 +0530

    Add files via upload

---
+++ Fact.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Fact{
+    public static void main(String[]args){
+        if(n==0)
+        return 1;
+        else
+        return n* factorial(int n){
+            
+        }
+    }
+}
